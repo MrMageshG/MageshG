73a910e8336994a2de74e33f0e3978f4ca427513
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-24 10:05:26 +0530

    Add files via upload

---
+++ Odd&Even.java
@@ -0,0 +1,28 @@
+\\put header file
+public static void main (String[] args) 
+
+{
+	
+int m;
+        
+Scanner s = new Scanner(System.in);
+        
+m = s.nextInt();
+        
+if(m % 2 == 0)
+        
+{
+            
+System.out.println("Even");
+        
+}
+        
+else
+        
+{
+            
+System.out.println("Odd");
+	
+}
+    
+}
