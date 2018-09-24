9a87713cf1d4ba410e055f6465397316d88a1c2c
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-24 14:18:24 +0530

    Create LpYr.java

---
+++ LpYr.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class LpYr
+{
+public class static void main(String[] args)
+{
+int yr;
+Scanner sc=new Scanner(System.in);
+ yr=sc.nextInt();
+if(yr % 4==0)
+{
+System.out.println("Yes");
+}
+else
+{
+	System.out.println("No");
+}
+}
+}
+
