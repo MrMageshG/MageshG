3280c075c7db5ff5a42a0200ca26236ac8c27c0f
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-24 12:10:14 +0530

    Create AlphandNo.java

---
+++ AlphandNo.java
@@ -0,0 +1,19 @@
+	import java.util.Scanner;
+  class AlphandNo
+  public static void main(Strng[] args)
+  {
+  char ch;
+        Scanner scan = new Scanner(System.in);
+		
+       ch = scan.next().charAt(0);
+		
+        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
+        {
+            System.out.print("Alphabet");
+        }
+        else
+        {
+            System.out.print("No");
+        }
+        
+    }
