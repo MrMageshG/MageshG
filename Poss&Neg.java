e7229869cb4aec3911abbb969e8f4ee785112da2
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-24 09:48:49 +0530

    Add files via upload

---
+++ Poss&Neg.java
@@ -0,0 +1,35 @@
+\\put header file
+publi static void main(String[] args)
+{
+int number;
+		
+Scanner s=new Scanner(System.in);
+		
+number=s.nextInt();
+        
+if(number > 0)
+        
+{
+            
+System.out.println("\nPositive ");
+        
+}
+        
+else if(number < 0)
+        
+{
+            
+System.out.println("\nNegative");
+        
+}
+
+        else
+        
+{
+            
+System.out.println(number+" is neither positive nor negative");
+        
+}
+	
+}
+
