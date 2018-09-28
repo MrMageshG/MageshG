4185a465579cc4444276dc53befd101c4f42e31b
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-28 15:09:51 +0530

    Add files via upload

---
+++ rvsNum.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class rvsNum
+{
+public static void main (String[] args) 	
+{
+		
+int r,sum=0,t;    
+  
+int n;
+ 
+Scanner s=new Scanner(System.in);
+ 
+System.out.println("Enter The Number:");
+ 
+n=s.nextInt();
+  
+t=n;    
+ 
+ while(n>0)
+{    
+   
+r=n%10;  
+   
+sum=(sum*10)+r;    
+   
+n=n/10;   
+
+  }
+    
+  if(t==sum)
+{    
+   
+System.out.println("yes ");  
+}  
+ 
+ else  
+{  
+   
+System.out.println("No");  
+}  
+
+}  
+
+}  
