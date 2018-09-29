3cde8429b477026104f79ff0f3f499c54c1e74e5
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-09-29 14:13:50 +0530

    Add files via upload

---
+++ PowerofNum.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+class PowerofNum
+{
+public static void main (String[] args)  
+
+{
+		
+int base , exponent ;
+		
+Scanner s=new Scanner(System.in);
+		base=s.nextInt();
+		
+exponent=s.nextInt();
+
+       
+ long result = 1;
+
+        
+while (exponent != 0)
+        
+{
+            
+result *= base;
+            
+--exponent;
+        
+}
+
+        
+System.out.println(result);
+   
+ }
+
+}
+	
