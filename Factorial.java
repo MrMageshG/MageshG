c982dfdd973d48478ef3fd0c255cae0fb692458e
MageshG <42763795+MrMageshG@users.noreply.github.com>
2018-12-19 11:01:55 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+  class Factorial
+double n,fact=1;
+	
+Scanner s=new Scanner(System.in);
+	
+n=s.nextDouble();
+
+ 
+if(n==0)
+	        
+{
+	        	
+System.out.println("1");
+	        
+}
+	        
+else
+	        
+{
+	        
+ 
+for(int i=1;i<=n;i++)
+	
+{
+		
+fact=fact*i;
+	
+}
+	
+System.out.println(+fact);
+	
+}
+
+}
+
+
+}
